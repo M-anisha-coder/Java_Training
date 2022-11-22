33fa3e93a440a07bced12cfa25dcf007a94e87e2
Manisha <66690707+M-anisha-coder@users.noreply.github.com>
2022-11-22 23:39:57 +0530

    Add files via upload

---
+++ sec-1_2_3/Challenge.java
@@ -0,0 +1,13 @@
+public class Challenge {
+    public static void main(String[] args) {
+
+        System.out.println("M   M     A  ");
+        System.out.println("MM MM    A A");
+        System.out.println("M M M   A   A");
+        System.out.println("M   M   AAAAA");
+        System.out.println("M   M   A   A");
+        System.out.println("M   M   A   A");
+        System.out.println("M   M   A   A");
+
+    }   
+}
